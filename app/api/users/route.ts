a471ed6e68244999c2545d13d025acea073a3e01
Amit Kumar Rout <akrout455@gmail.com>
2026-01-25 14:56:25 +0530

    added supabase

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,30 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import { pool } from '@/lib/db';
-import {auth,currentUser} from "@clerk/nextjs/server"
+import { auth, currentUser } from "@clerk/nextjs/server"
 
 export async function POST(req: NextRequest) {
   try {
-    console.log("working")
+    
     const user = await currentUser()
     const currUserId = user?.id
-   // console.log(user?.id)
-   // console.log(user)
-    await pool.query(
-        `INSERT INTO users (id, email) 
-        VALUES ($1, $2) 
-        ON CONFLICT (id) DO NOTHING`,
-        [currUserId, null] // Email can be added later if needed
-    );
-    return NextResponse.json({ message:"worknig" });
-   /* const { techStack, title } = await req.json();
-
-    // Ensure user exists in database
+    const currUserEmail = user?.emailAddresses[0].emailAddress
     await pool.query(
       `INSERT INTO users (id, email) 
-       VALUES ($1, $2) 
-       ON CONFLICT (id) DO NOTHING`,
-      [userId, null] // Email can be added later if needed
-    );*/
+        VALUES ($1, $2) 
+        ON CONFLICT (id) DO NOTHING`,
+      [currUserId, currUserEmail] // Email can be added later if needed
+    );
+    return NextResponse.json({ message: "user verified" });
+    /* const { techStack, title } = await req.json();
+ 
+     // Ensure user exists in database
+     await pool.query(
+       `INSERT INTO users (id, email) 
+        VALUES ($1, $2) 
+        ON CONFLICT (id) DO NOTHING`,
+       [userId, null] // Email can be added later if needed
+     );*/
 
   } catch (error: any) {
     console.error('Error creating chat:', error);
--- lib/db.ts
+++ lib/db.ts
@@ -7,6 +7,9 @@ const pool = process.env.DATABASE_URL
       max: 20,
       idleTimeoutMillis: 30000,
       connectionTimeoutMillis: 2000,
+     // ssl: {
+      //  rejectUnauthorized: false,
+      //},
     })
   : new Pool({
       host: process.env.DB_HOST || 'localhost',
